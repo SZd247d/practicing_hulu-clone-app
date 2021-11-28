7f3e0ccafd86e5d6bb496a31de1639e74578fb9b
zakisellali <zakisellali464@gmail.com>
2021-11-28 10:30:03 +0100

    initial commit-4

--- components/Nav.js
+++ components/Nav.js
@@ -19,7 +19,7 @@ function Nav() {
           </h2>
         ))}
       </div>
-      <div className="absolute top-0 right-0 bg-gradient-to-l from-white h-10 w-1/12"></div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12"></div>
     </nav>
   );
 }
